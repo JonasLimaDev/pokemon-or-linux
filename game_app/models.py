5bf3b93718559b4dce4aaf08fcdb69caced76347
Jonas Lima <jonas.lima.dev@gmail.com>
2025-01-30 15:21:12 -0300

    adição das classes em models.py

--- game_app/models.py
+++ game_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class DistroLinux(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    imagem = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Pokemon(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    imagem = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Jogador(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    pontuacao = models.IntegerField( null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
