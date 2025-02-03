58936d251a126928b087000ad311fcd4fd7739f3
Jonas Lima <jonas.lima.dev@gmail.com>
2025-02-03 18:15:35 -0300

    adicionado função para salvar nome e pontuação do jogador

--- game_app/static/js/scripts_game.js
+++ game_app/static/js/scripts_game.js
@@ -1,24 +1,35 @@
+const data = document.currentScript.dataset
+const csrf = data.csrf
+
 const getData = () => ({
     resultado: "",
+    myModal: new bootstrap.Modal('#exampleModal', {
+      keyboard: false
+    }),
+    
     nome_sorteado: "",
+    rodadas: 0,
+    jogo_finalizado: true,
     pokemon_click: false,
     distro_click: false,
     pontuacao:{
         resposta:'',
         pontos:0,
+        nomeJogador: '',
     },
     info_resposta:{
         nome:'',
         imagem:'',
     },
-    
+
 
     async sorteiaNome() {
+      
       await fetch('/game/api/sorteia_nome')
           .then((response) => response.json())
           .then(data => {
             this.nome_sorteado = data.name;
-          }) 
+          });
     },
 
     async pegarDadosDistro() {
@@ -38,6 +49,8 @@ const getData = () => ({
               this.info_resposta.imagem = data.imagem;
             });
       },
+    
+      
 
     async pontuar(){
         // pega qual o tipo do nome
@@ -56,6 +69,7 @@ const getData = () => ({
         else if (this.resultado != 'Distribuição Linux'  && this.distro_click){
             this.pontuacao.resposta = "Você Errou!!!";
             await this.pegarDadosPokemon();
+            this.jogo_finalizado = true
             console.log(this.info_resposta)
         }
         else if(this.resultado == 'Pokemon' && this.pokemon_click){
@@ -67,10 +81,35 @@ const getData = () => ({
         else if (this.resultado != 'Pokemon' && this.pokemon_click){
             this.pontuacao.resposta = "Você Errou!!!";
             await this.pegarDadosDistro();
+            this.jogo_finalizado = true
             console.log(this.info_resposta)
         }
         this.pokemon_click = false;
         this.distro_click = false;
+        // this.rodadas++;
+        // if(this.rodadas === 5){
+        //   this.jogo_finalizado = true
+        // }
+    },
+
+   async salvar_jogador(){
+      await fetch(
+        `/game/api/jogador`,
+        {
+          method: 'POST',
+          headers: {"Conten-Type": "application/json", "X-CSRFToken": csrf},
+          body: JSON.stringify({
+            'nome': this.pontuacao.nomeJogador,
+            'pontuacao': this.pontuacao.pontos,
+          })
+        }
+      )
+        .then((response) => response.json())
+          .then(data => {
+            this.resultado = data.resultado;
+          })
+          return window.location.replace("/")
+
     },
 
 });
